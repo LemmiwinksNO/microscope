fa6f1bdc1466fe263ffa8d8aa57f1840cc7aeefb
LemmiwinksNO <macreasy@gmail.com>
2014-04-11 06:39:51 -0500

    Modified comments for publications.js

--- server/publications.js
+++ server/publications.js
@@ -4,13 +4,13 @@ Meteor.publish('posts', function() {
 });
 
 // Publish comments for passed in postId
+// How does it get postId? It is passed from the subscription.
 Meteor.publish('comments', function(postId) {
  return db.comments.find({postId: postId});
 });
 
 // Return notifications for this user
 Meteor.publish('notifications', function() {
-  // publish function has current user's _id available
-  // as this.userId
+  // publish function has current user's _id available as this.userId
   return db.notifications.find({userId: this.userId});
 });
